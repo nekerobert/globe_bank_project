114a91bf29464a2d1b3756b279919f5f80191b3b
Robert Chukwuemeka <nekerobert@gmail.com>
2021-07-23 13:58:09 +0100

    Build navigation by creating list of subjects
    Add public_navigation.php file to private shared folder
    
    Modify public index.php by adding adding public_navigation path
    
    Modidy query_functions.php in private folder
    
    Add static_homepage.php file to private shared folder
    
    Add homepage_assets and page_assets folders containing images to the images folder

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
+<?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
   <div id="page">
+  <?php 
+  // show the home page
+  // The homepage content could:
+  // *be static content
+  // *show the first page from the nav
+  // *be in the database but add code to hide in the nav
+  
+  include(SHARED_PATH . '/static_homepage.php'); ?>
 
   </div>
 
